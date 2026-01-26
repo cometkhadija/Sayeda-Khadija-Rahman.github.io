5a3467f3610a759a4bf116ab672318e5a32a74a6
Sayeda Khadija Rahman <143632625+cometkhadija@users.noreply.github.com>
2026-01-26 14:15:04 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-
+window.addEventListener("scroll", () => {
+  const navbar = document.getElementById("navbar");
+  if (window.scrollY > 50) {
+    navbar.classList.add("scrolled");
+  } else {
+    navbar.classList.remove("scrolled");
+  }
+});
