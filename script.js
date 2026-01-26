60c8fc1c8c4ccf9f6b8956a07d104a1901c39cf5
Sayeda Khadija Rahman <sayedakhadijarahman@gmail.com>
2026-01-27 01:24:53 +0600

    project update

--- script.js
+++ script.js
@@ -6,3 +6,12 @@ window.addEventListener("scroll", () => {
     navbar.classList.remove("scrolled");
   }
 });
+
+document.querySelectorAll(".view-btn").forEach((btn) => {
+  btn.addEventListener("click", (e) => {
+    e.stopPropagation();
+    alert("Full project details will appear here later.");
+  });
+});
+
+
